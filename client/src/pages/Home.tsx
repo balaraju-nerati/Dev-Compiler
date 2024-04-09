af8a8a6668a2369dd3297e0e9693417a0ef9d779
balaraju-nerati <balrajnerati23@gmail.com>
2024-04-09 16:55:41 +0530

    completed compilation task of the Code Editor

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -2,6 +2,10 @@ import React from 'react'
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <div className='w-full h-[calc(100dvh-60px)] text-white flex flex-col justify-center items-center gap-4'>
+      <h1 className='text-6xl font-bold text-center'>Welcome to Dev-Compiler</h1>
+      <p className='text-gray-500 text-center text-lg'>Please login to build some stuff</p>
+
+    </div>
   )
 }
--- client/src/redux/slices/compilerSlice.ts
+++ client/src/redux/slices/compilerSlice.ts
@@ -1,16 +1,31 @@
 import { PayloadAction, createSlice } from "@reduxjs/toolkit"
 
 export interface CompilerSliceStateType {
-    html: string;
-    css: string;
-    javascript: string;
+    fullCode: {
+        html: string;
+        css: string;
+        javascript: string;
+    }
     currentLanguage: "html" | "css" | "javascript";
+    
 }
 
 const initialState: CompilerSliceStateType = {
-    html:"",
-    css:"",
-    javascript:"",
+    fullCode: {
+        html:`<html lang="en">
+  <body>
+    <div class="container">
+      <h1>Todo List</h1>
+      <input type="text" id="taskInput" placeholder="Add new task...">
+      <button id="addBtn">Add Task</button>
+      <ul id="taskList"></ul>
+    </div>
+    <script src="script.js"></script>
+  </body>
+</html>`,
+        css:"This is css code",
+        javascript:"This is javascript code",
+    },
     currentLanguage:"html"
 }
 
@@ -21,8 +36,12 @@ const compilerSlice = createSlice({
         updateCurrantLanguage:(state,action: PayloadAction<CompilerSliceStateType["currentLanguage"]>) =>{
             state.currentLanguage = action.payload;
         },
+        updateCodeValue: (state,action: PayloadAction<string>) =>{
+            state.fullCode[state.currentLanguage] = action.payload
+        },
+        
     },
 })
 
 export default compilerSlice.reducer
-export const {updateCurrantLanguage} = compilerSlice.actions
+export const {updateCurrantLanguage,updateCodeValue} = compilerSlice.actions
